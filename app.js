4d8360f3b38c9bc5ae880a5d22d288577556b8f9
AllyintheCode <allyasadzade@gmail.com>
2025-03-19 11:05:07 +0400

    home task complated

---
+++ app.js
@@ -0,0 +1,21 @@
+// let usaqlar = ["ali", "ferid", "murad"]
+// usaqlar.shift(0)
+// usaqlar.push("rza")
+// console.log(usaqlar);
+
+
+// let usaqlar = ["ali", "ferid", "murad"]
+// usaqlar.splice(1, 1, "CLASFIELD")
+// console.log(usaqlar);
+
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+// console.log(`episode4 ; ${arr.splice(0, 1)}`);
+// console.log(`episode5 ; ${arr.splice(0, 1)}`);
+// console.log(`episode6 ; ${arr.splice(0, 1)}`);
+
+
+// let reqemler = "32, 31, 34, 36, 31"
+// let teze = reqemler.replaceAll(', ', '; ')
+// console.log(teze);
+
